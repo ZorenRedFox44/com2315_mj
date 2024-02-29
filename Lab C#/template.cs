f56418b48c13b0f3db39c3b71b19f9711c478379
Germaine Domena <domenagermainej117@gmail.com>
2024-02-29 09:49:49 -0400

    c#

---
+++ Lab C#/template.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWold
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
