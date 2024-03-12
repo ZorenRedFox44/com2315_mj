3ea5cd5f08ae4dde77f64add9741f403b4e84c29
Germaine Domena <domenagermainej117@gmail.com>
2024-03-12 19:28:16 -0400

    EjeV02

---
+++ Lab C#/Ejemplo/EjeV02.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program{
+    static void Main(){
+        Console.Write("Ingrese un numero entro: ");
+        int entero = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese un numero flotante: ");
+        float flotante = float.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese una cadena de caracteres: ");
+        string cadena = Console.ReadLine();
+    }
+}
