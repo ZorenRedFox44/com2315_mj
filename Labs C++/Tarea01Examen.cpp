2ad0dbb5c1ab638cc8cbcc0c54344f58d1714bbf
Germaine Domena <domenagermainej117@gmail.com>
2024-03-19 14:39:24 -0400

    Tarea01Examen.cpp

---
+++ Labs C++/Tarea01Examen.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    double Examen1, Examen2, Examen3, Examen4,
+
+    cout << "Ingrese la calificación del primer examen: ";
+    cin >>Examen1;
+
+    cout << "Ingrese la calificación del primer examen: ";
+    cin >>Examen2;
+
+    cout << "Ingrese la calificación del primer examen: ";
+    cin >>Examen3;
+
+    cout << "Ingrese la calificación del primer examen: ";
+    cin >>Examen4;
+
+    double promedio = (Examen1, Examen2, Examen3, Examen4)
+
+    cout <<"El promedio final es"<<promedio<< <<endl;
+    
+    return 0;
+
+}
